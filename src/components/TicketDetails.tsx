f702b866121941dc175871888f9e521102c76f6f
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-07-24 17:36:10 +0800

    style: updated ticket details layout

--- src/components/TicketDetails.tsx
+++ src/components/TicketDetails.tsx
@@ -15,44 +15,52 @@ const TicketDetails = ({ padding, hasBorder = true }: TicketDetailsProps) => {
             <CardTitle className="flex justify-start">
                 <span className="text-primary text-2xl">Details</span>
             </CardTitle>
-            <CardDescription className="flex flex-row gap-2 items-center">
-                <span>ID:</span>
+
+            <section className="w-full flex flex-row gap-2">
+                <CardDescription className="flex-1 flex flex-row gap-2 items-center min-w-0">
+                    <span className="text-lg">ID:</span>
+                    <div className="flex-1 bg-muted border border-input rounded-xl py-0.5 px-4 overflow-x-auto">
+                        <span className="text-foreground text-lg whitespace-nowrap">{openedActionTicket?.id}</span>
+                    </div>
+                </CardDescription>
+                <CardDescription className="flex-1 flex flex-row gap-2 items-center min-w-0">
+                    <span className="text-lg">Status:</span>
+                    <div className="flex-1 bg-muted border border-input rounded-xl py-0.5 px-4 overflow-x-auto">
+                        <span className="text-foreground text-lg whitespace-nowrap">{openedActionTicket?.status}</span>
+                    </div>
+                </CardDescription>
+            </section>
+
+            <CardDescription className="flex flex-row gap-2 items-center min-w-0">
+                <span className="text-lg">Title:</span>
                 <div className="flex-1 bg-muted border border-input rounded-xl py-0.5 px-4 overflow-x-auto">
-                    <span className="text-foreground text-lg whitespace-nowrap">{openedActionTicket?.id}</span>
-                </div>
-            </CardDescription>
-            <CardDescription className="flex flex-row gap-2 items-center">
-                <span>Title:</span>
-                <div className="flex-1 bg-muted border border-input rounded-xl py-0.5 px-4 overflow-x-auto">
-                    <span className="text-foreground text-lg whitespace-nowrap">{openedActionTicket?.title}</span>
-                </div>
-            </CardDescription>
-            <CardDescription className="flex flex-row gap-2 items-center">
-                <span>Status:</span>
-                <div className="flex-1 bg-muted border border-input rounded-xl py-0.5 px-4 overflow-x-auto">
-                    <span className="text-foreground text-lg whitespace-nowrap">{openedActionTicket?.status}</span>
+                    <span className="text-foreground text-lg whitespace-nowrap">
+                        {openedActionTicket?.title}lkjljlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkj
+                    </span>
                 </div>
             </CardDescription>
             <CardDescription className="min-h-0 flex-1 flex flex-row gap-2">
-                <span>Description:</span>
+                <span className="text-lg">Description:</span>
                 <div className="overflow-y-auto bg-muted border border-input rounded-xl py-0.5 px-4">
                     <span className="text-foreground text-lg">{openedActionTicket?.description}</span>
                 </div>
             </CardDescription>
+
             <CardDescription className="flex flex-row gap-2 items-center">
-                <span>Created at:</span>
+                <span className="text-lg">Created at:</span>
                 <div className="flex-1 bg-muted border border-input rounded-xl py-0.5 px-4 overflow-x-auto">
                     <span className="text-foreground text-lg whitespace-nowrap">{openedActionTicket?.created_at}</span>
                 </div>
             </CardDescription>
-            <CardDescription className="flex flex-row gap-2 items-center">
-                <span>Updated at:</span>
+            <CardDescription className="flex flex-row gap-2 items-center min-w-0">
+                <span className="text-lg">Updated at:</span>
                 <div className="flex-1 bg-muted border border-input rounded-xl py-0.5 px-4 overflow-x-auto">
                     <span className="text-foreground text-lg whitespace-nowrap">{openedActionTicket?.updated_at}</span>
                 </div>
             </CardDescription>
+
             <CardDescription className="flex flex-row gap-2 items-center">
-                <span>Assigned to:</span>
+                <span className="text-lg">Assigned to:</span>
                 <div className="flex-1 overflow-x-auto flex flex-row items-center bg-muted border border-input rounded-xl py-0.5 px-4">
                     <span className="text-foreground text-lg whitespace-nowrap">
                         {openedActionTicket?.assigned_to.name}
