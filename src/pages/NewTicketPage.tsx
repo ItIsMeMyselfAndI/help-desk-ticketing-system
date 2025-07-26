7b9481484283d8cc2e5f96853372926bddfa867a
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-07-26 20:41:15 +0800

    feat: add uploaded files list card
    Added working scrollbar x & y
    Added delete button

--- src/pages/NewTicketPage.tsx
+++ src/pages/NewTicketPage.tsx
@@ -1,7 +1,8 @@
 import React, { useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { UploadIcon } from "lucide-react";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Trash2Icon, UploadIcon } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 type DropAreaProps = {
     updateFiles: (files: File[]) => void;
@@ -69,7 +70,7 @@ const DropArea = ({ updateFiles }: DropAreaProps) => {
                         isInside && "bg-muted"
                     }`}
                 >
-                    <span className="text-primary text-2xl">Drag & Drop Files</span>
+                    <span className="text-primary text-2xl font-semibold">Drag & Drop</span>
                     <UploadIcon className="size-12 text-primary" strokeWidth={2} />
                     {/* file uploads */}
                     <input
@@ -79,8 +80,11 @@ const DropArea = ({ updateFiles }: DropAreaProps) => {
                         onChange={handleFilesSelected}
                         className="hidden"
                     ></input>
-                    <Button onClick={handleBrowseClick} className="text-foreground rounded-full px-6">
-                        <span className="text-foreground text-xl">Browse Files</span>
+                    <Button
+                        onClick={handleBrowseClick}
+                        className="text-foreground rounded-full px-6 hover:bg-primary/80"
+                    >
+                        <span className="text-foreground text-xl font-semibold">Browse Files</span>
                     </Button>
                 </div>
             </Card>
@@ -88,10 +92,44 @@ const DropArea = ({ updateFiles }: DropAreaProps) => {
     );
 };
 
-const UploadedFiles = () => {
+type UploadedFilesProps = {
+    uploadedFiles: File[];
+};
+
+const UploadedFiles = ({ uploadedFiles }: UploadedFilesProps) => {
     return (
         <>
-            <Card className="flex-2"></Card>
+            <Card className="flex-2 flex flex-col gap-0 p-0 min-h-0">
+                <CardTitle className="flex justify-start p-4">
+                    <span className="text-primary text-2xl">Uploaded Files</span>
+                </CardTitle>
+                <Separator orientation="horizontal" />
+                <CardContent className="min-h-0 p-4 flex">
+                    <div className="flex-1 overflow-y-auto flex flex-col gap-2">
+                        {uploadedFiles.length === 0 ? (
+                            <section className="h-full flex justify-center items-center">
+                                <span>No files yet</span>
+                            </section>
+                        ) : (
+                            uploadedFiles.map((file) => (
+                                <div key={file.name} className="flex items-center gap-2">
+                                    <div className="flex-1 min-w-0 flex py-2 px-4 rounded-xl border border-accent bg-muted">
+                                        <section className="h-auto overflow-x-auto flex flex-row justify-center items-center">
+                                            <span className="whitespace-nowrap text-foreground text-lg font-normal">
+                                                {file.name}
+                                                lskjflskjflsklsjflsjfljflskjflskdjflskdjflksdjflskdjflksjdfjflskdjflskdjflkdsjflkjs
+                                            </span>
+                                        </section>
+                                    </div>
+                                    <Button className="size-auto rounded-xl p-3 bg-red-500 hover:p-4 hover:bg-red-500">
+                                        <Trash2Icon className="size-5 text-foreground m-0 p-0" />
+                                    </Button>
+                                </div>
+                            ))
+                        )}
+                    </div>
+                </CardContent>
+            </Card>
         </>
     );
 };
@@ -129,9 +167,9 @@ const NewTicketPage = () => {
             <form className="flex-8">
                 <Card className="size-full"></Card>
             </form>
-            <aside className="flex-5 max-w-lg flex flex-col gap-4">
+            <aside className="flex-5 min-w-0 max-w-lg flex flex-col gap-4">
                 <DropArea updateFiles={updateFiles} />
-                <UploadedFiles />
+                <UploadedFiles uploadedFiles={uploadedFiles} />
             </aside>
         </main>
     );
