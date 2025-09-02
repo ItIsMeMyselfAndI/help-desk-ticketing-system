5654472b71fb1a242ad38826cc07a3e2069c99bb
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-09-02 16:30:50 +0800

    chore(test): simplify user db update tests - explicitly enumerate test cases instead of loops

--- backend/tests/test_db_crud/test_user/test_update.py
+++ backend/tests/test_db_crud/test_user/test_update.py
@@ -13,26 +13,34 @@ class TestDBUpdateTicket(unittest.TestCase):
     def setUp(self):
         self.db = next(get_db())
         # reset db
-        reset_db(bind=self.db.get_bind(), datasets_path="app/datasets.json", limit=1)
-        # json datasets
-        with open("app/datasets.json", "r") as file:
-            self.sample_users = json.load(file)["users"]
+        reset_db(bind=self.db.get_bind())
+        self.test_user_dict = {
+            "username": "new",
+            "email": "new@gmail.com",
+            "password": "123",
+            "role": constants.UserRole.SUPPORT,
+        }
+        self.existing_user_dict = {
+            "username": "old",
+            "email": "old@gmail.com",
+            "password": "123",
+            "role": constants.UserRole.CLIENT,
+        }
+        self.existing_user, _ = crud.create_user(
+            self.db, schemas.UserCreate.model_validate(self.existing_user_dict)
+        )
 
     def tearDown(self):
         # just to make sure
         self.db.close()  # not necessary since get_db closes it on success/fail
 
     def test_invalid_arg(self):
-        user_id = self.db.execute(select(models.User.id).limit(1)).scalars().first()
-        if not user_id:
-            self.skipTest("empty users table")
+        if self.existing_user is None:
+            self.skipTest("existing user was not created")
         invalid_args = ["lksdjfd", None, 4.8]
-        user_update = schemas.UserUpdate(
-            username="old",
-            email="old@gmail.com",
-            password="123",
-            role=constants.UserRole.CLIENT,
-        )
+        user_id = self.existing_user.id
+        user_update = schemas.UserUpdate.model_validate(self.test_user_dict)
+
         for arg in invalid_args:
             # db session
             with self.subTest(arg=arg, user_id=user_id, user_update=user_update):
@@ -48,107 +56,89 @@ class TestDBUpdateTicket(unittest.TestCase):
                     crud.update_user(self.db, user_id, arg)
 
     def test_existing_username(self):
-        user_id = self.db.execute(select(models.User.id).limit(1)).scalars().first()
-        if not user_id:
-            self.skipTest("empty users table")
-        user_update = schemas.UserUpdate(
-            username="old",
-            email="old@gmail.com",
-            password="123",
-            role=constants.UserRole.CLIENT,
-        )
-        # dummy
-        result_1 = crud.update_user(self.db, user_id, user_update)
-        if result_1[1] != constants.StatusCode.SUCCESS:
-            self.skipTest("dummy user not updated")
-        # test
-        user_update.email = "new@gmail.com"
-        result_2 = crud.update_user(self.db, user_id, user_update)
-        self.assertEqual(result_2[1], constants.StatusCode.UNAME_ALREADY_EXIST)
+        if self.existing_user is None:
+            self.skipTest("existing user was not created")
+        user_id = self.existing_user.id
+        user_update = schemas.UserUpdate.model_validate(self.test_user_dict)
+        user_update.username = "old"
+
+        result_user, status_code = crud.update_user(self.db, user_id, user_update)
+        self.assertIsNone(result_user, "user w/ existing username was created")
+        self.assertEqual(status_code, constants.StatusCode.UNAME_ALREADY_EXIST)
 
     def test_existing_email(self):
-        user_id = self.db.execute(select(models.User.id).limit(1)).scalars().first()
-        if not user_id:
-            self.skipTest("empty users table")
-        user_update = schemas.UserUpdate(
-            username="old",
-            email="old@gmail.com",
-            password="123",
-            role=constants.UserRole.CLIENT,
-        )
-        # dummy
-        result_1 = crud.update_user(self.db, user_id, user_update)
-        if result_1[1] != constants.StatusCode.SUCCESS:
-            self.skipTest("dummy user not updated")
-        # test
-        user_update.username = "new"
-        result_2 = crud.update_user(self.db, user_id, user_update)
-        self.assertEqual(result_2[1], constants.StatusCode.EMAIL_ALREADY_EXIST)
+        if self.existing_user is None:
+            self.skipTest("existing user was not created")
+        user_id = self.existing_user.id
+        user_update = schemas.UserUpdate.model_validate(self.test_user_dict)
+        user_update.email = "old@gmail.com"
 
-    def test_without_dates(self):
-        user_id = self.db.execute(select(models.User.id).limit(1)).scalars().first()
-        if not user_id:
-            self.skipTest("empty users table")
-        user_dict = {
-            "username": "name",
-            "email": "name@gmail.com",
-            "password": "123",
-            "role": constants.UserRole.SUPPORT,
-        }
-        user_update = schemas.UserUpdate(**user_dict)
-        result = crud.update_user(self.db, user_id, user_update)
-        if not result[0]:
-            self.fail("valid user with dates not updated")
-        del user_dict["password"]  # no password in models.User.as_dict()
-        for key in user_dict.keys():  # stop sub test if not eq
-            with self.subTest(key=key, result=result):
-                self.assertEqual(result[0].as_dict()[key], user_dict[key])
+        result_user, status_code = crud.update_user(self.db, user_id, user_update)
+        self.assertIsNone(result_user, "user w/ existing email was created")
+        self.assertEqual(status_code, constants.StatusCode.EMAIL_ALREADY_EXIST)
 
     def test_optional_field_on_user_update_basemodel(self):
-        user_id = self.db.execute(select(models.User.id).limit(1)).scalars().first()
-        if not user_id:
-            self.skipTest("empty users table")
-        optional_fields = [
-            {"username": "name"},
-            {"email": f"name@gmail.com"},
-            {"role": constants.UserRole.SUPPORT},
-            {"password": "123"},
-        ]
-        for field in optional_fields:
-            with self.subTest(field=field):
-                user_update = schemas.UserUpdate.model_validate(field)
-                self.assertIsInstance(user_update, schemas.UserUpdate)
+        if self.existing_user is None:
+            self.skipTest("existing user was not created")
+
+        # uname
+        user_update = schemas.UserUpdate.model_validate({"username": "new"})
+        self.assertIsInstance(user_update, schemas.UserUpdate)
+        # email
+        user_update = schemas.UserUpdate.model_validate({"email": "new@gmail.com"})
+        self.assertIsInstance(user_update, schemas.UserUpdate)
+        # pass
+        user_update = schemas.UserUpdate.model_validate({"password": "123"})
+        self.assertIsInstance(user_update, schemas.UserUpdate)
+        # role
+        user_update = schemas.UserUpdate.model_validate(
+            {"role": constants.UserRole.SUPPORT}
+        )
+        self.assertIsInstance(user_update, schemas.UserUpdate)
 
     def test_invalid_value_on_user_update_basemodel(self):
-        user_id = self.db.execute(select(models.User.id).limit(1)).scalars().first()
-        if not user_id:
-            self.skipTest("empty users table")
-        invalid_value = [1, 2, 3]
-        keys = [
-            "username",
-            "email",
-            "role",
-            "password",
-        ]
-        for key in keys:
-            with self.subTest(key=key):
-                with self.assertRaises(pydantic.ValidationError):
-                    schemas.UserUpdate.model_validate({key: invalid_value})
+        # uname
+        test_user_dict = self.test_user_dict.copy()
+        test_user_dict["username"] = [1, 2, 3]
+        with self.assertRaises(pydantic.ValidationError):
+            schemas.UserUpdate.model_validate(test_user_dict)
+        # email
+        test_user_dict = self.test_user_dict.copy()
+        test_user_dict["email"] = [1, 2, 3]
+        with self.assertRaises(pydantic.ValidationError):
+            schemas.UserUpdate.model_validate(test_user_dict)
+        # pass
+        test_user_dict = self.test_user_dict.copy()
+        test_user_dict["password"] = [1, 2, 3]
+        with self.assertRaises(pydantic.ValidationError):
+            schemas.UserUpdate.model_validate(test_user_dict)
+        # role
+        test_user_dict = self.test_user_dict.copy()
+        test_user_dict["role"] = [1, 2, 3]
+        with self.assertRaises(pydantic.ValidationError):
+            schemas.UserUpdate.model_validate(test_user_dict)
 
     def test_none_in_optional_field_on_user_update_basemodel(self):
-        user_id = self.db.execute(select(models.User.id).limit(1)).scalars().first()
-        if not user_id:
-            self.skipTest("empty users table")
-        keys = [
-            "username",
-            "email",
-            "role",
-            "password",
-        ]
-        for key in keys:
-            with self.subTest(key=key):
-                user_update = schemas.UserUpdate.model_validate({key: None})
-                self.assertIsInstance(user_update, schemas.UserUpdate)
+        # uname
+        test_user_dict = self.test_user_dict.copy()
+        test_user_dict["username"] = None
+        user_update = schemas.UserUpdate.model_validate(test_user_dict)
+        self.assertIsInstance(user_update, schemas.UserUpdate)
+        # email
+        test_user_dict = self.test_user_dict.copy()
+        test_user_dict["email"] = None
+        user_update = schemas.UserUpdate.model_validate(test_user_dict)
+        self.assertIsInstance(user_update, schemas.UserUpdate)
+        # pass
+        test_user_dict = self.test_user_dict.copy()
+        test_user_dict["password"] = None
+        user_update = schemas.UserUpdate.model_validate(test_user_dict)
+        self.assertIsInstance(user_update, schemas.UserUpdate)
+        # role
+        test_user_dict = self.test_user_dict.copy()
+        test_user_dict["role"] = None
+        user_update = schemas.UserUpdate.model_validate(test_user_dict)
+        self.assertIsInstance(user_update, schemas.UserUpdate)
 
 
 if __name__ == "__main__":
